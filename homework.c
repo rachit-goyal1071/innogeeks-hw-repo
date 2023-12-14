77e263f0963804014607b3d91373ea4f908ec0c0
ajayraj004 <ajayrajospf2@gmail.com>
2023-12-15 01:22:03 +0530

    add Question 2

--- homework.c
+++ homework.c
@@ -1,19 +1,45 @@
 #include<stdio.h>
 
+//Question 1
+// int main()
+// {
+//     int i,sum=0,num;
+//     printf("Enter the num : ");
+//     scanf("%d",&num);
+
+//     for(i=2; i<=num; i+=2){   // i+=2 for addition of 2 =
+
+//          sum += i;
+
+//     }
+//     printf("%d",sum);
+
+    
+//     return 0;
+
+// }
+
+// Question2
+
+
 int main()
 {
-    int i,sum=0,num;
+    int num;
     printf("Enter the num : ");
     scanf("%d",&num);
 
-    for(i=2; i<=num; i+=2){   // i+=2 for addition of 2 =
+        if(num%3==0)
+        printf("Fizz");
+        
+        else if (num%5==0)
+        printf("Buzz");
 
-         sum += i;
+        else if(num%3==0 && num%5==0 )
+        printf("FizzBuzz");
 
-    }
-    printf("%d",sum);
-
-    
+        else
+        printf("Entry is other than 3 and 5 which is : %d ",num);
+        
     return 0;
-
 }
+
